958f31a33563e0c64816097611585d0c0626edb8
Omay <70179858+Omay238@users.noreply.github.com>
2022-12-03 03:45:53 +0000

    nunjucks

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const path = require("path");
+const nunjucks = require("nunjucks");
 const express = require("express");
 const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, "static")));
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.render("index.njk");
 });
 
 app.listen(port, () => {
